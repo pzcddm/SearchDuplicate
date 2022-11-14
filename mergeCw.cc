638e33e8cb4b27e290c549cec19da74c39709c92
Zhencan Peng <zp128@ilab3.cs.rutgers.edu>
2022-11-13 23:04:54 -0500

    writing mergeCW

--- mergeCw.cc
+++ mergeCw.cc
@@ -6,6 +6,9 @@ using namespace std;
 #include "util/IO.hpp"
 #include "util/util.hpp"
 
+const int BUFFER_SIZE = 1e8;
+int buffer[BUFFER_SIZE];
+
 int main(){
     string scattered_dir = "./openwebtext_50K_64k_50T_8M_50257ZP_SCATTERED/";
     string merged_dir = "openwebtext_50K_64k_50T_8M_50257ZP";
@@ -46,6 +49,7 @@ int main(){
 
     // Start Iterate each hash function's scattered data
     printf("----------- starting merging--------------\n")
+
     for(int i = 0 ;i<k;i++){
         auto local_st = LogTime();
 
@@ -67,6 +71,58 @@ int main(){
             zmp_ifstreams[j].open(tmp_zmp_file, ios::in | ios::binary);
         }
 
+        unsigned long long global_offset = 0;
+        unsigned long long cur_token_offset;
+
+        vector<pair<int,unsigned long long> zonemap_buffer;
+        // Pratically Merging
+        for(int j =0 ;j<tokenNum;j++){
+            // thet starting offset of current token in the merged_cws_file
+            cur_token_offset = global_offset;
+            int cur_window_num = 0;
+            for(int k = 0;k<scattered_num;k++){
+                int tmp_windowsNum = index_vec[k][i][j];
+
+                // read cws and write them
+                cws_ifstreams[k].read((char *)buffer, sizeof(CW)*tmp_windowsNum );
+                merged_cws_file.write((char *)buffer, sizeof(CW)*tmp_windowsNum );
+
+                
+
+                // read Zonemap and save it into Zonemap_buffer
+                unsigned tid;
+                unsigned zp_len;
+                zmp_ifstreams[k].read((char *)&tid, sizeof(unsigned));    // read token_id
+                zmp_ifstreams[k].read((char *)&zp_len, sizeof(unsigned)); // read zonemap_length
+                assert(tid == j );
+                for(int l = 0;l<zp_len;l++){
+                    int text_id; unsigned long long offset;
+                    zmp_ifstreams[k].read((char *)&text_id, sizeof(int));  
+                    zmp_ifstreams[k].read((char *)&offset, sizeof(unsigned long long));
+                    offset += global_offset;
+                    zonemap_buffer.emplace_back(text_id,offset);
+                }
+
+                // update variables
+                global_offset += sizeof(CW)*tmp_windowsNum;
+                cur_window_num += tmp_windowsNum;
+            }
+
+            // Write zonemap files
+            unsigned cur_token = j;
+            unsigned cur_zp_len =zonemap_buffer.size();
+            merged_zonemap_ofstream.write((char *)&cur_token, sizeof(unsigned))
+            merged_zonemap_ofstream.write((char *)&cur_zp_len, sizeof(unsigned))
+            for (auto const &pir : zonemap_buffer) {
+                zpFile.write((char *)&pir.first, sizeof(int));                 // write text_id
+                zpFile.write((char *)&pir.second, sizeof(unsigned long long)); // write offset
+            }
+
+            // Record indexItem
+            merged_indexArr[i][j].windowsNum = cur_window_num;
+            merged_indexArr[i][j].offset = cur_token_offset;
+        }
+
         // Close ofstream and ifstream
         for(int j =0;j<scattered_num;j++){
             cws_ifstreams[j].close();
@@ -74,16 +130,18 @@ int main(){
         }
         merged_cws_ofstream.close();
         merged_zonemap_ofstream.close()
-    }
-    printf("------------------Writing Index File------------------\n");
 
-    ofstream outFile(index_file, ios::out | ios::binary);
+        printf("Local Merging of current hash function cost : %f\n", RepTime(local_st));
+    }
+
+    printf("------------------Writing Index File------------------\n");
+    ofstream index_ofstream(merged_index_file, ios::out | ios::binary);
     for (int i = 0; i < k; i++) {
         for (int j = 0; j < tokenNum; j++) {
-            outFile.write((char *)&indexArr[i][j], sizeof(IndexItem));
+            index_ofstream.write((char *)&merged_indexArr[i][j], sizeof(IndexItem));
         }
     }
-    outFile.close();
+    index_ofstream.close();
     printf("------------------Index File Writed------------------\n");
 
     printf("Total cost : %f\n", RepTime(global_st));
